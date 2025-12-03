c22c04ba445f1396f5079a847d8e543c14d14ded
Jeremias Pedro <gracimasakigraci@gmail.com>
2025-12-03 22:10:25 +0100

    Actualizei

--- script/script.js
+++ script/script.js
@@ -301,11 +301,3 @@
       loginModal.style.transition ='.4s'
     },100)
   })
-
-  const modal_form = document.querySelectorAll('.modal_form')
-  modal_form.forEach((form)=>{
-    form.addEventListener('submit',(e)=>{
-      e.preventDefault()
-      window.open('index2.html','_self')
-    })
-  })
